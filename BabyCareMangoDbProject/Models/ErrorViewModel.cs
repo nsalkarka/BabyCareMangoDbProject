650f0b57390c164418a7e86f717427e96a19ad9e
nsalkarka <_karka67@windowslive.com>
2025-03-04 20:15:23 +0300

    Add project files.

---
+++ BabyCareMangoDbProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BabyCareMangoDbProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
